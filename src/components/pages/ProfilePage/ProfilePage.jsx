a30762353299b4eb3162c5353c11469e83d43a4a
Sergio <seregaant71@gmail.com>
2025-04-19 23:11:51 +0300

    update styles profilePage

--- src/components/pages/ProfilePage/ProfilePage.jsx
+++ src/components/pages/ProfilePage/ProfilePage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Header from "../../common/Header/Header";
 import BottomNav from "../../common/BottomNav/BottomNav";
-import styles from './ProfilePage.module.css';
 import ProfileMain from "./ProfileMain/ProfileMain";
+import styles from "../RecommendationsPage/RecommendationsPage.module.css";
 
 const ProfilePage = () => {
     return (
-        <div>
-            <Header className={styles.header}>Профиль пользователя</Header>
+        <div className={styles.page}>
+            <Header title={'Профиль'}></Header>
             <ProfileMain />
             <BottomNav />
         </div>
